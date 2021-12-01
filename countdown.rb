42b417988feb3e2e52e7d4da6c0a56a783b0b4b9
Darian <smith.darian2015@gmail.com>
2021-11-30 23:28:08 -0600

    commit tree 0241665f33e1cc2c3ba61217e938cf5b1332d759

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
-end
+
+def countdown(count)
+    count = 10
+    while count > 0
+      puts "#{count} SECOND(S)!"
+      count-=1
+    end
+    "HAPPY NEW YEAR!"
+  end
+
+  def countdown_with_sleep(count)
+    while count > 0
+      count-=1
+      sleep(1)
+    end
+      puts "HAPPY NEW YEAR!"
+  end
